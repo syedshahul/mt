3cd72001f3211fa0e7b21ff293387517125cb17a
syedshahul <syedshahul2002@gmail.com>
2014-03-14 18:14:26 +0530

    initial amqp setup

--- mt-ws/src/main/java/com/loycl/mt/rest/SampleRest.java
+++ mt-ws/src/main/java/com/loycl/mt/rest/SampleRest.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
  * @author: Syed Shahul
  */
 @Path("samplerst") @Produces(
-	{MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON,
+	{MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 	 MediaType.APPLICATION_ATOM_XML})
 public interface SampleRest {
 	@GET Response getSample() throws MTException;
